b7581e816c33808fc05e768fabd365c58181c119
Sergio Robles <sroblesc@gmail.com>
2019-12-18 18:02:06 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -3,12 +3,14 @@ require 'directors_database'
 
 #pp directors_database
 
-def directors_totals(nds)
-  # Remember, it's always OK to pretty print what you get *in* to make sure
+# Remember, it's always OK to pretty print what you get *in* to make sure
   # that you know what you're starting with!
   #
   #
   # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
+
+def directors_totals(nds)
+  
   result = {
   }
   #
